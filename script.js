ba41b25f276b74fe03363a15d33a3e6754cac7ed
IbrahimHassan502 <ih35840@gmail.com>
2023-07-11 14:53:23 +0200

    commit

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 "use strict";
-function arrayDiff(a, b) {
-  let numArr = [];
-  a.forEach((aNum) => {
-    let add = 1;
-    b.forEach((bNum) => (aNum === bNum ? (add = 0) : ""));
-    add ? numArr.push(aNum) : "";
-  });
-  return numArr;
-}
-console.log(arrayDiff([1, 2, 2], [1]));
+import { searchCategories as srchCat } from "./data.js";
+
+srchCat.forEach((cat) => {
+  console.log(cat.replace("&", "and").split(" ").join("-"));
+});
